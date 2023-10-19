069185905c25f16102873386426045cd43f0d27d
kocha-koch <nastya/kochneva68@gmail.com>
2023-10-19 16:10:44 +0300

    4 lab

--- main.js
+++ main.js
@@ -1,127 +1,136 @@
-//  //задание 1
-// function convertSpeed(v, dir) {
-//     if (dir=='toMS') {
-//         return(`${v} м/c км/ч соответствует ${v/3.6} м/c`); 
-//     }
-//     else if (dir=='toKMH') {
-//         return(`${v} м/c соответствует ${v*3.6} км/ч`);
-//     }
-//     else return "Ошибочное значение";
-// }
+const div = document.querySelector('.threejs');
 
-// console.log(convertSpeed(36, 'toMS'));
-// console.log(convertSpeed(10, 'toKMH'));
-
-// //задание 2
-// console.log("");
-// function absValue(n) {
-//     if (n>=0) {
-//         return n;
-//     }
-//     return -n;
-// }
-// console.log(absValue(-2));
-// console.log(absValue(100.1));
-// console.log(absValue(0));
-
-// //задание 3
-// console.log("");
-
-// let student = {
-//     group: 324,
-//     last_name: "Кочнева",
-//     first_name: "Анастасия",
-//     };
-// console.log("Список свойств: ", Object.keys(student));
-// console.log(`Студент ${student.last_name} ${student.first_name} учится в ${student.group} группе`);
-
-
-// //задание 4
-// console.log("");
-// function randomNumber(min,max) {
-//         min = Math.ceil(min);
-//         max = Math.floor(max);
-//         return Math.floor(Math.random() * (max - min + 1)) + min;
-//       }
-// console.log(randomNumber(-10,10));
-// console.log(randomNumber(0,10));
-
-// //задание 5
-// console.log("");
-// function sampleArray (arr, k) {
-//     let randomArr = [];
-//     for (let i=0; i<k; i++) {
-//         randomArr.push(arr[randomNumber(0,arr.length-1)]);
-//     }
-//     return randomArr;
-// }
-// console.log(sampleArray([1,9,3,4], 10));
-// console.log(sampleArray([4,8,5,7], 2));
-
-const openModal = document.querySelector('.openModal')
-const modal = document.querySelector('.modal')
-const closeModal = document.querySelector('.closeModal')
-const checkPass = document.querySelector('.checkPass')
-const temp = document.getElementById("pass")
-const input = document.querySelector("input")
-const mail = document.getElementById("mail")
-const error = document.getElementById("error")
-const form = document.getElementById("form")
-const pass = document.getElementById("pass")
-
-openModal.addEventListener('click', () =>{
-    modal.showModal()
+document.forms[0].addEventListener('change', (e) => {
+    cube.material.color.set(e.target.value);
 })
 
-closeModal.addEventListener('click', () =>{
-    modal.close()
+document.forms[1].addEventListener('change', (e) => {
+ pyramid.material.color.set(e.target.value);
 })
 
-modal.addEventListener('click', (e) =>{
-    if(e.target === modal) modal.close()
+document.forms[2].addEventListener('change', (e) => {
+ hemiLight.intensity=e.target.value;
 })
 
-checkPass.addEventListener('pointerdown', () =>{
-    temp.type = "text"
-})
-checkPass.addEventListener('pointerup', () =>{
-    temp.type = "password"
+document.forms[3].addEventListener('change', (e) => {
+    directionalLight.intensity=e.target.value;
 })
 
-
-mail.addEventListener("blur", () => {
-    if (mail.validity.typeMismatch){
-        error.textContent = "Почта введена некорректно!"
-        mail.setCustomValidity("Ошибка!")
-    }
-
-    else if (mail.validity.valueMissing){
-        error.textContent = "Пожалуйста, введите значения"
-        mail.setCustomValidity("Ошибка!")
-    }
-    else{
-        error.textContent = ""
-        mail.setCustomValidity("")
-    }
+document.forms[4].addEventListener('change', (e) => {
+    directionalLight1.intensity=e.target.value;
 })
 
-pass.addEventListener("blur", () => {
-    if (pass.validity.tooShort) {
-        error.textContent = "Должно быть минимум 6 символов!"
-        pass.setCustomValidity("Ошибка!")
-    }
-    else if (pass.validity.valueMissing){
-        error.textContent = "Пропущены значения!"
-        pass.setCustomValidity("Ошибка!")
-    }
-    else
-    {
-        error.textContent = ""
-        pass.setCustomValidity("")
-    }
-}) 
+window.addEventListener('resize', onWindowResize);
+function onWindowResize() {
+    camera.aspect=div.clientWidth/div.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(div.clientWidth, div.clientHeight);
+}
 
-form.addEventListener("submit", (e) =>{
-    console.table({email: mail.value, password: pass.value});
-    e.preventDefault()
-})
+let scene= new THREE.Scene();
+
+let camera = new THREE.PerspectiveCamera(50, div.clientWidth / div.clientHeight, 0.1, 100);
+camera.position.set(3, 0.7, 3);
+const renderer = new THREE.WebGLRenderer({ antialias: true,div});
+renderer.setSize(div.clientWidth, div.clientHeight);
+div.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
+
+scene.background = new THREE.Color('black');
+scene.fog = new THREE.Fog('black', 1, 5);
+
+let hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemiLight.position.set(0, 200, 0);
+scene.add(hemiLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(-10, 12, 9);
+directionalLight.castShadow = true;
+
+scene.add(directionalLight);
+
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight1.position.set(10, 12, 9);
+directionalLight1.castShadow = true;
+
+scene.add(directionalLight1);
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(4000, 4000),
+    new THREE.MeshPhongMaterial({ color:0x808080, dithering: true })
+);
+plane.rotation.x = - Math.PI / 2;
+plane.receiveShadow = true;
+scene.add(plane);
+
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material = new THREE.MeshPhongMaterial({color: ''});  
+const cube = new THREE.Mesh(geometry, material);
+cube.castShadow=true;
+cube.position.set(1.7, 0.5, 0)
+scene.add(cube);
+
+var geometry1 = new THREE.BufferGeometry();
+const vertices1 = new Float32Array( [
+    -2.0, -2.0, 2.0,
+    2.0, -2.0, 2.0,
+    2.0, 2.0, 2.0,
+
+    2.0, 2.0, 2.0,
+    -2.0, 2.0, 2.0,
+    -2.0, -2.0, 2.0
+] );
+
+geometry1.setAttribute('position', new THREE.Float32BufferAttribute( vertices1, 3 ));
+geometry1.computeVertexNormals();
+
+const material1 = new THREE.MeshPhongMaterial( { color: 'Gray', side:THREE.DoubleSide } );
+const mesh1 = new THREE.Mesh( geometry1, material1 );
+mesh1.receiveShadow=true;
+mesh1.position.x=2.4;
+mesh1.position.z=-2.4;
+scene.add( mesh1 );
+
+
+var geometry2 = new THREE.BufferGeometry();
+const vertices2 = new Float32Array( [
+    0, 0, 0.5,
+    0.5, 0,  0,
+    -0.25, -Math.sqrt(0.5*0.5-0.25*0.25),  0,
+
+    0, 0, 0.5,
+    0.5, 0, 0,
+    -0.25, Math.sqrt(0.5*0.5-0.25*0.25),  0,
+
+    0, 0, 0.5,
+    -0.25,  -Math.sqrt(0.5*0.5-0.25*0.25),  0,
+    -0.25,  Math.sqrt(0.5*0.5-0.25*0.25),  0,
+
+    0.5, 0,  0,
+    -0.25,  -Math.sqrt(0.5*0.5-0.25*0.25),  0,
+    -0.25,  Math.sqrt(0.5*0.5-0.25*0.25),  0
+] );
+geometry2.setAttribute('position', new THREE.Float32BufferAttribute( vertices2, 3 ));
+geometry2.computeVertexNormals();
+const material2 = new THREE.MeshPhongMaterial( { color: '', side:THREE.DoubleSide } );
+const pyramid = new THREE.Mesh( geometry2, material2 );
+pyramid.castShadow=true;
+pyramid.position.set(3.3, 0.5, 0.2)
+scene.add( pyramid );
+
+function render(time) {
+    time *= 0.001; 
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    pyramid.rotation.x = time;
+    pyramid.rotation.z = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+}
+ requestAnimationFrame(render);
+
+ 
